8bb6ef62605ffb9926ce07bc7531fdc5deae9b07
imranq2 <imranq2@hotmail.com>
2020-09-20 13:50:21 -0700

    Add MANIFEST.in

---
+++ CONTRIBUTING.md
@@ -0,0 +1,20 @@
+# SparkPipelineFramework
+Framework for simpler Spark Pipelines
+
+
+# Publishing package 
+For credentials:
+
+Set up your $HOME/.pypirc file like this (replace password with real one):
+
+```
+[pypi]
+  username = __token__
+  password = pypi-AgEIcHlwaS5vcmcCJDU5YTg1ZDZjLTVhOWItNGZmMi1hMTBhLTgzZjVhMzBlYmJhOAACJXsicGVybWlzc2lvbnMiOiAidXNlciIsICJ2ZXJzaW9uIjogMX0AAAYgUAfdyImgcqvyNbLihu22g4Wp_2SYZvvJDx7iYNJpEUg
+```
+
+Then:
+
+Increment version in VERSION file
+
+Run ```make package```
--- setup.py
+++ setup.py
@@ -1,21 +1,33 @@
-import setuptools
+from setuptools import setup, find_packages
+from os import path, getcwd
 
 # from https://packaging.python.org/tutorials/packaging-projects/
+
+package_name = 'sparkpipelineframework'
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="sparkpipelineframework",
-    version="0.0.1",
+try:
+    with open(path.join(getcwd(), 'VERSION')) as version_file:
+        version = version_file.read().strip()
+except IOError:
+    raise
+
+# classifiers: https://pypi.org/classifiers/
+
+setup(
+    name=package_name,
+    version=version,
     author="Imran Qureshi",
     author_email="imranq2@hotmail.com",
     description="Framework for simpler Spark Pipelines",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/imranq2/SparkPipelineFramework",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     classifiers=[
+        "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
