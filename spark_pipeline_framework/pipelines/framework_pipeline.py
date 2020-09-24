4a4dff7fdc61b1dfcaba51b0f18c8cf3b86d9565
imranq2 <imranq2@hotmail.com>
2020-09-24 13:15:29 -0700

    add framework_pipeline.py

--- spark_pipeline_framework/pipelines/framework_pipeline.py
+++ spark_pipeline_framework/pipelines/framework_pipeline.py
@@ -1,3 +1,51 @@
-class FrameworkPipeline:
-    def __init__(self):
-        pass
+from typing import List, Optional
+
+from pyspark.ml.base import Transformer
+from pyspark.sql.dataframe import DataFrame
+
+from spark_pipeline_framework.logger.yarn_logger import get_logger
+from spark_pipeline_framework.progress_logger.progress_log_metric import ProgressLogMetric
+from spark_pipeline_framework.progress_logger.progress_logger import ProgressLogger
+from spark_pipeline_framework.utilities.FriendlySparkException import FriendlySparkException
+from spark_pipeline_framework.utilities.attr_dict import AttrDict
+
+
+class FrameworkPipeline(Transformer):
+    def __init__(self,
+                 parameters: AttrDict,
+                 progress_logger: ProgressLogger
+                 ):
+        super(FrameworkPipeline, self).__init__()
+        self.transformers: List[Transformer] = []
+        self.parameters: AttrDict = parameters
+        self.progress_logger: ProgressLogger = progress_logger
+
+    # noinspection PyUnusedLocal
+    def fit(self, df: DataFrame) -> 'FrameworkPipeline':
+        return self
+
+    def _transform(self, df: DataFrame) -> DataFrame:
+        logger = get_logger(__name__)
+        count_of_transformers: int = len(self.transformers)
+        i: int = 0
+        for transformer in self.transformers:
+            stage_name: Optional[str] = None
+            try:
+                i += 1
+                if hasattr(transformer, "getName"):
+                    # noinspection Mypy
+                    stage_name = transformer.getName()
+                    logger.info(f"---- Running transformer {stage_name}  ({i} of {count_of_transformers}) ----")
+                    # self.spark_session.sparkContext.setJobDescription(stage_name)
+                    # print_memory_stats(sc(df))
+                with ProgressLogMetric(progress_logger=self.progress_logger, name=stage_name or "unknown"):
+                    df = transformer.transform(dataset=df)
+            except Exception as e:
+                logger.warn(f"======== stage threw exception =======")
+                if hasattr(transformer, "getSql"):
+                    # noinspection Mypy
+                    logger.info(transformer.getSql())
+                logger.warn(f"======== stage threw exception =======")
+                # use exception chaining to add stage name but keep original exception
+                raise FriendlySparkException(str(e), stage_name=stage_name)
+        return df
---
+++ tests/pipelines/framework_pipeline/test_can_run_framework_pipeline.py
@@ -0,0 +1,59 @@
+from pathlib import Path
+
+from pyspark.ml.pipeline import Pipeline
+from pyspark.sql.dataframe import DataFrame
+from pyspark.sql.session import SparkSession
+from pyspark.sql.types import StructType
+
+from library.features.carriers.features_carriers import FeaturesCarriers
+from spark_pipeline_framework.pipelines.framework_pipeline import FrameworkPipeline
+from spark_pipeline_framework.progress_logger.progress_logger import ProgressLogger
+from spark_pipeline_framework.transformers.framework_csv_loader import FrameworkCsvLoader
+from spark_pipeline_framework.utilities.attr_dict import AttrDict
+from spark_pipeline_framework.utilities.flattener import flatten
+
+
+class MyPipeline(FrameworkPipeline):
+    def __init__(self, parameters: AttrDict, progress_logger: ProgressLogger):
+        super(MyPipeline, self).__init__(parameters=parameters,
+                                         progress_logger=progress_logger)
+        self.transformers = flatten([
+            [
+                FrameworkCsvLoader(
+                    view="flights",
+                    path_to_csv=parameters["flights_path"]
+                )
+            ],
+            FeaturesCarriers(parameters=parameters).transformers,
+        ])
+
+
+def test_can_run_framework_pipeline(spark_session: SparkSession) -> None:
+    # Arrange
+    data_dir: Path = Path(__file__).parent.joinpath('./')
+    flights_path: str = f"file://{data_dir.joinpath('flights.csv')}"
+
+    schema = StructType([])
+
+    df: DataFrame = spark_session.createDataFrame(
+        spark_session.sparkContext.emptyRDD(), schema)
+
+    spark_session.sql("DROP TABLE IF EXISTS default.flights")
+
+    # Act
+    parameters = AttrDict(
+        {
+            "flights_path": flights_path
+        }
+    )
+
+    with ProgressLogger() as progress_logger:
+        pipeline: MyPipeline = MyPipeline(parameters=parameters, progress_logger=progress_logger)
+        transformer = pipeline.fit(df)
+        transformer.transform(df)
+
+    # Assert
+    result_df: DataFrame = spark_session.sql("SELECT * FROM flights2")
+    result_df.show()
+
+    assert result_df.count() > 0
