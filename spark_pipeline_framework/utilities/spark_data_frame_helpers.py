7b60bf0189351c94105f930a8b8d2bee65f592d2
imranq2 <imranq2@hotmail.com>
2020-10-05 15:04:32 -0700

    add some more spark helper functions

--- spark_pipeline_framework/utilities/spark_data_frame_helpers.py
+++ spark_pipeline_framework/utilities/spark_data_frame_helpers.py
@@ -1,5 +1,6 @@
 from typing import Dict, Any, List
 
+from pyspark import SparkContext, SQLContext
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.types import StructType
 
@@ -21,3 +22,45 @@ def create_empty_dataframe(spark_session: SparkSession) -> DataFrame:
         spark_session.sparkContext.emptyRDD(), schema)
 
     return df
+
+
+def create_dataframe_from_json(
+        spark_session: SparkSession,
+        schema: StructType,
+        json: str) -> DataFrame:
+    return spark_session.read.schema(schema).json(spark_session.sparkContext.parallelize([json]))
+
+
+def spark_is_data_frame_empty(df: DataFrame) -> bool:
+    """
+    Efficient way to check if the data frame is empty without getting the count of the whole data frame
+    """
+    # from: https://stackoverflow.com/questions/32707620/how-to-check-if-spark-dataframe-is-empty
+    return not bool(df.head(1))
+
+
+def spark_get_execution_plan(df: DataFrame, extended: bool = False):
+    if extended:
+        # noinspection PyProtectedMember
+        return df._jdf.queryExecution().toString()
+    else:
+        # noinspection PyProtectedMember
+        return df._jdf.queryExecution().simpleString()
+
+
+def spark_table_exists(sql_ctx: SQLContext, view: str) -> bool:
+    # noinspection PyBroadException
+    return view in sql_ctx.tableNames()
+
+
+def sc(df: DataFrame) -> SparkContext:
+    # noinspection PyProtectedMember
+    return df._sc
+
+
+def add_metadata_to_column(df: DataFrame, column: str, metadata: Any) -> DataFrame:
+    return df.withColumn(column, df[column].alias(column, metadata=metadata))
+
+
+def get_metadata_of_column(df: DataFrame, column: str) -> Any:
+    return df.select(column).schema[0].metadata
