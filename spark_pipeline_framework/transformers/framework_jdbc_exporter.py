e2fed53dab3a347b648cca97d570dd32977d4540
Alex Hearn <alex.d.hearn@gmail.com>
2020-10-29 08:37:47 -0500

    Add JDBC exporter; refactor parquet exporter to subclass BaseExporter

---
+++ spark_pipeline_framework/transformers/framework_jdbc_exporter.py
@@ -0,0 +1,70 @@
+from pathlib import Path
+from typing import Dict, Any, Union
+
+# noinspection PyProtectedMember
+from pyspark import keyword_only
+from pyspark.ml.param import Param
+
+from spark_pipeline_framework.logger.yarn_logger import get_logger
+from spark_pipeline_framework.transformers.framework_base_exporter import FrameworkBaseExporter
+
+
+class FrameworkJdbcExporter(FrameworkBaseExporter):
+
+    # noinspection PyUnusedLocal
+    @keyword_only
+    def __init__(self, jdbc_url: str, table: str, driver: str, **kwargs):
+        super().__init__(**kwargs)
+        assert jdbc_url
+        assert table
+        assert driver
+
+        self.logger = get_logger(__name__)
+
+        self.jdbc_url: Param = Param(self, "jdbc_url", "")
+        self._setDefault(jdbc_url=jdbc_url)  # type: ignore
+
+        self.table: Param = Param(self, "table", "")
+        self._setDefault(table=table)  # type: ignore
+
+        self.driver: Param = Param(self, "driver", "")
+        self._setDefault(driver=driver)  # type: ignore
+
+        self._set(jdbc_url=jdbc_url, table=table, driver=driver)
+
+    # noinspection PyPep8Naming,PyMissingOrEmptyDocstring
+    def setJdbcUrl(self, value) -> 'FrameworkJdbcExporter':
+        self._paramMap[self.jdbc_url] = value  # type: ignore
+        return self
+
+    # noinspection PyPep8Naming,PyMissingOrEmptyDocstring
+    def getJdbcUrl(self) -> Union[str, Path]:
+        return self.getOrDefault(self.jdbc_url)  # type: ignore
+
+    # noinspection PyPep8Naming,PyMissingOrEmptyDocstring
+    def setTable(self, value) -> 'FrameworkJdbcExporter':
+        self._paramMap[self.table] = value  # type: ignore
+        return self
+
+    # noinspection PyPep8Naming,PyMissingOrEmptyDocstring
+    def getTable(self) -> Union[str, Path]:
+        return self.getOrDefault(self.table)  # type: ignore
+
+    # noinspection PyPep8Naming,PyMissingOrEmptyDocstring
+    def setDriver(self, value) -> 'FrameworkJdbcExporter':
+        self._paramMap[self.driver] = value  # type: ignore
+        return self
+
+    # noinspection PyPep8Naming,PyMissingOrEmptyDocstring
+    def getDriver(self) -> Union[str, Path]:
+        return self.getOrDefault(self.driver)  # type: ignore
+
+    def getFormat(self) -> str:
+        return "jdbc"
+
+    def getOptions(self) -> Dict[str, Any]:
+        return {
+            "url": self.getJdbcUrl(),
+            "dbtable": self.getTable(),
+            "driver": self.getDriver(),
+        }
--- spark_pipeline_framework/transformers/framework_parquet_exporter.py
+++ spark_pipeline_framework/transformers/framework_parquet_exporter.py
@@ -4,96 +4,27 @@ from typing import Dict, Any, Union
 # noinspection PyProtectedMember
 from pyspark import keyword_only
 from pyspark.ml.param import Param
-from pyspark.sql.dataframe import DataFrame
-from pyspark.sql.utils import AnalysisException
 
 from spark_pipeline_framework.logger.yarn_logger import get_logger
-from spark_pipeline_framework.progress_logger.progress_log_metric import ProgressLogMetric
-from spark_pipeline_framework.progress_logger.progress_logger import ProgressLogger
-from spark_pipeline_framework.transformers.framework_transformer import FrameworkTransformer
+from spark_pipeline_framework.transformers.framework_base_exporter import FrameworkBaseExporter
 
 
-class FrameworkParquetExporter(FrameworkTransformer):
+class FrameworkParquetExporter(FrameworkBaseExporter):
+
     # noinspection PyUnusedLocal
     @keyword_only
-    def __init__(
-        self,
-        file_path: Union[str, Path],
-        view: str = None,
-        name: str = None,
-        parameters: Dict[str, Any] = None,
-        progress_logger: ProgressLogger = None,
-        limit: int = -1
-    ):
-        super().__init__(
-            name=name, parameters=parameters, progress_logger=progress_logger
-        )
+    def __init__(self, file_path: Union[str, Path], **kwargs):
+        super().__init__(**kwargs)
 
         assert isinstance(file_path, Path) or isinstance(file_path, str)
-
         assert file_path
 
         self.logger = get_logger(__name__)
 
-        self.view: Param = Param(self, "view", "")
-        self._setDefault(view=view)  # type: ignore
-
         self.file_path: Param = Param(self, "file_path", "")
         self._setDefault(file_path=None)  # type: ignore
 
-        self.limit: Param = Param(self, "limit", "")
-        self._setDefault(limit=None)  # type: ignore
-
-        kwargs = self._input_kwargs  # type: ignore
-        self.setParams(**kwargs)
-
-    # noinspection PyPep8Naming,PyMissingOrEmptyDocstring, PyUnusedLocal
-    @keyword_only
-    def setParams(
-        self,
-        file_path: Union[str, Path],
-        view: str = None,
-        name: str = None,
-        parameters: Dict[str, Any] = None,
-        progress_logger: ProgressLogger = None,
-        limit: int = -1
-    ):
-        kwargs = self._input_kwargs  # type: ignore
-        super().setParams(
-            name=name, parameters=parameters, progress_logger=progress_logger
-        )
-        return self._set(**kwargs)  # type: ignore
-
-    def _transform(self, df: DataFrame) -> DataFrame:
-        view: str = self.getView()
-        path: Union[str, Path] = self.getFilePath()
-        name: str = self.getName()
-        progress_logger: ProgressLogger = self.getProgressLogger()
-        # limit: int = self.getLimit()
-
-        with ProgressLogMetric(
-            name=f"{name or view}_table_loader",
-            progress_logger=progress_logger
-        ):
-            try:
-                if view:
-                    df.sql_ctx.table(view).write.parquet(path=str(path))
-                else:
-                    df.write.parquet(path=str(path))
-
-            except AnalysisException as e:
-                self.logger.error(f"File write failed to {path}")
-                raise e
-        return df
-
-    # noinspection PyPep8Naming,PyMissingOrEmptyDocstring
-    def setView(self, value) -> 'FrameworkParquetExporter':
-        self._paramMap[self.view] = value  # type: ignore
-        return self
-
-    # noinspection PyPep8Naming,PyMissingOrEmptyDocstring
-    def getView(self) -> str:
-        return self.getOrDefault(self.view)  # type: ignore
+        self._set(file_path=file_path)
 
     # noinspection PyPep8Naming,PyMissingOrEmptyDocstring
     def setFilePath(self, value) -> 'FrameworkParquetExporter':
@@ -104,11 +35,8 @@ class FrameworkParquetExporter(FrameworkTransformer):
     def getFilePath(self) -> Union[str, Path]:
         return self.getOrDefault(self.file_path)  # type: ignore
 
-    # noinspection PyPep8Naming,PyMissingOrEmptyDocstring
-    def setLimit(self, value) -> 'FrameworkParquetExporter':
-        self._paramMap[self.limit] = value  # type: ignore
-        return self
+    def getFormat(self) -> str:
+        return "parquet"
 
-    # noinspection PyPep8Naming,PyMissingOrEmptyDocstring
-    def getLimit(self) -> int:
-        return self.getOrDefault(self.limit)  # type: ignore
+    def getOptions(self) -> Dict[str, Any]:
+        return {"path": self.getFilePath()}
