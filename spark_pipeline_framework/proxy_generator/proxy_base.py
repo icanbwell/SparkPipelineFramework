eea31a83e1de5eb455bed90aa88ec66e0355a7d6
saurabh <saurabh.singh@joshtechnologygroup.com>
2020-11-06 17:57:48 +0530

    reformating

--- spark_pipeline_framework/proxy_generator/proxy_base.py
+++ spark_pipeline_framework/proxy_generator/proxy_base.py
@@ -51,7 +51,8 @@ class ProxyBase:
             elif file.endswith('.csv') and self.loader is None:
                 file_name = file.replace('.csv', '')
                 self.loader = FrameworkCsvLoader(
-                    view=file_name, path_to_csv=path.join(self.location, file),
+                    view=file_name,
+                    path_to_csv=path.join(self.location, file),
                     delimiter=parameters.get("delimiter", ",")
                 )
             elif file == 'convert.sql':
