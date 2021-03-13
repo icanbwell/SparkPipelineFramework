687ec321b5a13302aa99bce0d95b00c1412461d8
imranq2 <imranq2@hotmail.com>
2021-03-13 08:07:16 -0800

    add id to slack event handler

--- spark_pipeline_framework/event_loggers/slack_logger/v1/slack_event_logger.py
+++ spark_pipeline_framework/event_loggers/slack_logger/v1/slack_event_logger.py
@@ -6,6 +6,7 @@ from spark_pipeline_framework.utilities.slack_client import SlackClient
 class SlackEventLogger(EventLogger):
     def __init__(
         self,
+        id_: str,
         slack_token: str,
         slack_channel: str,
         bot_user_name: str,
@@ -14,6 +15,7 @@ class SlackEventLogger(EventLogger):
         """
         Logs events to Slack
 
+        :param id_: unique id for this logger
         :param slack_token: token to auth with Slack
         :param slack_channel: channel to post message into
         :param bot_user_name: user name to use when posting messages
@@ -23,6 +25,7 @@ class SlackEventLogger(EventLogger):
         assert slack_token
         assert slack_channel
         assert bot_user_name
+        self.id_: str = id_
         self.slack_token: str = slack_token
         self.slack_channel: str = slack_channel
         self.slack_user_name: str = bot_user_name
@@ -32,13 +35,19 @@ class SlackEventLogger(EventLogger):
         )
 
     def log_progress_event(
-        self, event_name: str, current: int, total: int, event_format_string: str
+        self,
+        event_name: str,
+        current: int,
+        total: int,
+        event_format_string: str,
+        backoff: bool = True,
     ) -> None:
         # for first 10 batches send every time
         # for 10 to 100 send every 10 batches
         # after 100 send every 100 batches
         if (
             not self.backoff
+            or not backoff
             or (current < 10)
             or (current < 100 and current % 10 == 0)
             or (current < 1000 and current % 100 == 0)
@@ -47,14 +56,14 @@ class SlackEventLogger(EventLogger):
             or (current < 1000000 and current % 100000 == 0)
         ):
             self.slack_client.post_message_to_slack(
-                event_format_string.format(event_name, current, total)
+                f"{self.id_}: " + event_format_string.format(event_name, current, total)
             )
 
     def log_event(self, event_name: str, event_text: str) -> None:
-        self.slack_client.post_message_to_slack(event_text)
+        self.slack_client.post_message_to_slack(f"{self.id_}: " + event_text)
 
     def log_exception(self, event_name: str, event_text: str, ex: Exception) -> None:
         # don't send full exception to slack since it can have PHI
         self.slack_client.post_message_to_slack(
-            f"{event_name}: {event_text} {type(ex)}"
+            f"{self.id_}: " + f"{event_name}: {event_text} {type(ex)}"
         )
