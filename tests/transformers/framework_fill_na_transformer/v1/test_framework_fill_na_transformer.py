4131f8f22d0fb4053fd0db74cd2ec51107585185
sean-can-bwell <sean@icanbwell.com>
2021-03-26 15:20:09 -0700

    updated test

--- tests/transformers/framework_fill_na_transformer/v1/test_framework_fill_na_transformer.py
+++ tests/transformers/framework_fill_na_transformer/v1/test_framework_fill_na_transformer.py
@@ -1,12 +1,15 @@
-from os import path, makedirs
 from pathlib import Path
-from shutil import rmtree
 
 from pyspark.sql import SparkSession, DataFrame
-from spark_pipeline_framework.progress_logger.progress_logger import ProgressLogger
-from spark_pipeline_framework.transformers.framework_csv_loader.v1.framework_csv_loader import FrameworkCsvLoader
-from spark_pipeline_framework.utilities.spark_data_frame_helpers import create_empty_dataframe
-from spark_pipeline_framework.transformers.framework_fill_na_transformer.v1.framework_fill_na_transformer import FrameworkFillNaTransformer
+from spark_pipeline_framework.transformers.framework_csv_loader.v1.framework_csv_loader import (
+    FrameworkCsvLoader,
+)
+from spark_pipeline_framework.utilities.spark_data_frame_helpers import (
+    create_empty_dataframe,
+)
+from spark_pipeline_framework.transformers.framework_fill_na_transformer.v1.framework_fill_na_transformer import (
+    FrameworkFillNaTransformer,
+)
 
 
 def test_framework_fill_na_transformer(spark_session: SparkSession) -> None:
@@ -36,7 +39,7 @@ def test_framework_fill_na_transformer(spark_session: SparkSession) -> None:
     assert 7 == result_df.count()
 
     # ensure that no rows are dropped when there are no null values
-    FrameworkDropRowsWithNullTransformer(
+    FrameworkFillNaTransformer(
         columns_to_check=["NPI", "Last Name"], view=view
     ).transform(result_df)
     assert 1111111111 == result_df.select("NPI").sum()
