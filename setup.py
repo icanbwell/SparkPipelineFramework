b5c2209a23d55d607bda09fc81fe8d247f54bdb4
imranq2 <imranq2@hotmail.com>
2021-05-03 20:01:24 -0700

    set python_requires to >=3.6

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.6",
     dependency_links=[],
     include_package_data=True,
     zip_safe=False,
