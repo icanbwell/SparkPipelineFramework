1eed254c5979f088b5fb30e54af558795b6b08e5
imranq2 <imranq2@hotmail.com>
2021-05-04 22:34:40 -0700

    add framework_if_else_transformer

---
+++ spark_pipeline_framework/transformers/framework_if_else_transformer/v1/framework_if_else_transformer.py
@@ -0,0 +1,66 @@
+from typing import Dict, Any, Optional, Union, List, Callable
+
+# noinspection PyProtectedMember
+from pyspark import keyword_only
+from pyspark.ml import Transformer
+from pyspark.sql.dataframe import DataFrame
+from spark_pipeline_framework.logger.yarn_logger import get_logger
+from spark_pipeline_framework.progress_logger.progress_logger import ProgressLogger
+from spark_pipeline_framework.transformers.framework_transformer.v1.framework_transformer import (
+    FrameworkTransformer,
+)
+
+
+class FrameworkIfElseTransformer(FrameworkTransformer):
+    # noinspection PyUnusedLocal
+    @keyword_only
+    def __init__(
+        self,
+        enable: Union[bool, Callable[[DataFrame], bool]],
+        stages: Union[List[Transformer], Callable[[], List[Transformer]]],
+        else_stages: Optional[
+            Union[List[Transformer], Callable[[], List[Transformer]]]
+        ] = None,
+        name: Optional[str] = None,
+        parameters: Optional[Dict[str, Any]] = None,
+        progress_logger: Optional[ProgressLogger] = None,
+    ):
+        """
+        If enable flag is true then runs stages else runs else_stages
+        :param enable: a boolean or a function that takes a DataFrame and returns a boolean
+        :param stages: list of transformers or a function that returns a list of transformers
+        :param else_stages: list of transformers or a function that returns a list of transformers
+        """
+        super().__init__(
+            name=name, parameters=parameters, progress_logger=progress_logger
+        )
+
+        self.logger = get_logger(__name__)
+
+        self.enable: Union[bool, Callable[[DataFrame], bool]] = enable
+
+        self.stages: Union[List[Transformer], Callable[[], List[Transformer]]] = stages
+        self.else_stages: Optional[
+            Union[List[Transformer], Callable[[], List[Transformer]]]
+        ] = else_stages
+
+        kwargs = self._input_kwargs
+        self.setParams(**kwargs)
+
+    def _transform(self, df: DataFrame) -> DataFrame:
+        enable = self.enable if isinstance(self.enable, bool) else self.enable(df)
+        if enable:
+            stages: List[Transformer] = self.stages if isinstance(
+                self.stages, list
+            ) else self.stages()
+        else:
+            stages = (
+                []
+                if self.else_stages is None
+                else self.else_stages
+                if isinstance(self.else_stages, list)
+                else self.else_stages()
+            )
+        for stage in stages:
+            df = stage.transform(df)
+        return df
--- spark_pipeline_framework/utilities/run_if_else.py
+++ spark_pipeline_framework/utilities/run_if_else.py
@@ -1,36 +1,26 @@
-import inspect
 from typing import List, Optional, Union, Callable
 
 from pyspark.ml import Transformer
+from pyspark.sql.dataframe import DataFrame
+
+from spark_pipeline_framework.transformers.framework_if_else_transformer.v1.framework_if_else_transformer import (
+    FrameworkIfElseTransformer,
+)
 
 
 def run_if_else(
-    enable: bool,
+    enable: Union[bool, Callable[[DataFrame], bool]],
     stages: Union[List[Transformer], Callable[[], List[Transformer]]],
     else_stages: Optional[
         Union[List[Transformer], Callable[[], List[Transformer]]]
     ] = None,
-) -> List[Transformer]:
+) -> FrameworkIfElseTransformer:
     """
-    If enable is true then it returns stages otherwise it returns else_stages
-    (If stages or else_stages is a function then it evaluates it and returns the result
-
-
-
-    :param enable: whether to return stages or else_stages
-    :param stages:
-    :param else_stages:
-    :return:
+        If enable flag is true then runs stages else runs else_stages
+        :param enable: a boolean or a function that takes a DataFrame and returns a boolean
+        :param stages: list of transformers or a function that returns a list of transformers
+        :param else_stages: list of transformers or a function that returns a list of transformers
     """
-    if enable:
-        if isinstance(stages, list):
-            return stages
-        assert inspect.isfunction(stages)
-        return stages()
-    else:
-        if isinstance(else_stages, list):
-            return else_stages
-        if not else_stages:
-            return []
-        assert inspect.isfunction(else_stages)
-        return else_stages()
+    return FrameworkIfElseTransformer(
+        enable=enable, stages=stages, else_stages=else_stages
+    )
