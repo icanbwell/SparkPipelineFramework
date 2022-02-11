a92da318e86245c7f108220a8a8237d998501976
Michael Vidal <michael.vidal@icanbwell.com>
2022-02-11 09:38:22 -0500

    move validation dataframe writing to finally block

--- spark_pipeline_framework/pipelines/v2/framework_pipeline.py
+++ spark_pipeline_framework/pipelines/v2/framework_pipeline.py
@@ -73,74 +73,78 @@ class FrameworkPipeline(Transformer):
         return self
 
     def _transform(self, df: DataFrame) -> DataFrame:
-        # if steps are defined but not transformers then convert steps to transformers first
-        if len(self.steps) > 0 and len(self.transformers) == 0:
-            self.transformers = self.create_steps(self.steps)
-        # get the logger to use
-        logger = get_logger(__name__)
-        count_of_transformers: int = len(self.transformers)
-        i: int = 0
-        pipeline_name: str = self.__class__.__name__
-        self.progress_logger.log_event(
-            event_name=pipeline_name, event_text=f"Starting Pipeline {pipeline_name}"
-        )
-        for transformer in self.transformers:
-            assert isinstance(transformer, Transformer), type(transformer)
-            try:
-                i += 1
-                logger.info(
-                    f"---- Running pipeline [{pipeline_name}] transformer [{transformer}]  "
-                    f"({i} of {count_of_transformers}) ----"
-                )
-
-                with ProgressLogMetric(
-                    progress_logger=self.progress_logger,
-                    name=str(transformer) or "unknown",
-                ):
-                    self.progress_logger.log_event(
-                        pipeline_name, event_text=f"Running pipeline step {transformer}"
+        try:
+            # if steps are defined but not transformers then convert steps to transformers first
+            if len(self.steps) > 0 and len(self.transformers) == 0:
+                self.transformers = self.create_steps(self.steps)
+            # get the logger to use
+            logger = get_logger(__name__)
+            count_of_transformers: int = len(self.transformers)
+            i: int = 0
+            pipeline_name: str = self.__class__.__name__
+            self.progress_logger.log_event(
+                event_name=pipeline_name,
+                event_text=f"Starting Pipeline {pipeline_name}",
+            )
+            for transformer in self.transformers:
+                assert isinstance(transformer, Transformer), type(transformer)
+                try:
+                    i += 1
+                    logger.info(
+                        f"---- Running pipeline [{pipeline_name}] transformer [{transformer}]  "
+                        f"({i} of {count_of_transformers}) ----"
                     )
-                    df = transformer.transform(dataset=df)
-            except Exception as e:
-                if hasattr(transformer, "getName"):
-                    # noinspection Mypy
-                    stage_name = transformer.getName()  # type: ignore
-                else:
-                    stage_name = transformer.__class__.__name__
-                logger.error(
-                    f"!!!!!!!!!!!!! pipeline [{pipeline_name}] transformer [{stage_name}] threw exception !!!!!!!!!!!!!"
-                )
-                # use exception chaining to add stage name but keep original exception
-                friendly_spark_exception: FriendlySparkException = (
-                    FriendlySparkException(exception=e, stage_name=stage_name)
-                )
-                error_messages: List[str] = (
-                    friendly_spark_exception.message.split("\n")
-                    if friendly_spark_exception.message
-                    else []
-                )
-                for error_message in error_messages:
-                    logger.error(msg=error_message)
 
-                if hasattr(transformer, "getSql"):
-                    # noinspection Mypy
-                    logger.error(transformer.getSql())  # type: ignore
-                logger.error(
-                    "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-                )
-                self.progress_logger.log_exception(
-                    event_name=pipeline_name,
-                    event_text=f"Exception in Stage={stage_name}",
-                    ex=e,
-                )
-                raise friendly_spark_exception from e
+                    with ProgressLogMetric(
+                        progress_logger=self.progress_logger,
+                        name=str(transformer) or "unknown",
+                    ):
+                        self.progress_logger.log_event(
+                            pipeline_name,
+                            event_text=f"Running pipeline step {transformer}",
+                        )
+                        df = transformer.transform(dataset=df)
+                except Exception as e:
+                    if hasattr(transformer, "getName"):
+                        # noinspection Mypy
+                        stage_name = transformer.getName()  # type: ignore
+                    else:
+                        stage_name = transformer.__class__.__name__
+                    logger.error(
+                        f"!!!!!!!!!!!!! pipeline [{pipeline_name}] transformer [{stage_name}] threw exception !!!!!!!!!!!!!"
+                    )
+                    # use exception chaining to add stage name but keep original exception
+                    friendly_spark_exception: FriendlySparkException = (
+                        FriendlySparkException(exception=e, stage_name=stage_name)
+                    )
+                    error_messages: List[str] = (
+                        friendly_spark_exception.message.split("\n")
+                        if friendly_spark_exception.message
+                        else []
+                    )
+                    for error_message in error_messages:
+                        logger.error(msg=error_message)
 
-        self._validate(df)
+                    if hasattr(transformer, "getSql"):
+                        # noinspection Mypy
+                        logger.error(transformer.getSql())  # type: ignore
+                    logger.error(
+                        "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+                    )
+                    self.progress_logger.log_exception(
+                        event_name=pipeline_name,
+                        event_text=f"Exception in Stage={stage_name}",
+                        ex=e,
+                    )
+                    raise friendly_spark_exception from e
 
-        self.progress_logger.log_event(
-            event_name=pipeline_name, event_text=f"Finished Pipeline {pipeline_name}"
-        )
-        return df
+            self.progress_logger.log_event(
+                event_name=pipeline_name,
+                event_text=f"Finished Pipeline {pipeline_name}",
+            )
+            return df
+        finally:
+            self._validate(df)
 
     def _validate(self, df: DataFrame) -> None:
         tables_df = df.sql_ctx.tables().filter(
