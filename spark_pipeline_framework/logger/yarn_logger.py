5ec63712ca9379aeb974c67ad33f25db9aeb138c
imranq2 <imranq2@hotmail.com>
2022-03-17 22:25:57 -0700

    improve formatting for logs

--- spark_pipeline_framework/logger/yarn_logger.py
+++ spark_pipeline_framework/logger/yarn_logger.py
@@ -1,10 +1,12 @@
 from os import environ
 from sys import stderr
 from logging import Logger, getLogger, StreamHandler, Formatter, INFO
-from typing import Union, TextIO
+from typing import Union, TextIO, Optional
 
 
-def get_logger(name: str, level: Union[int, str] = INFO) -> Logger:
+def get_logger(
+    name: str, level: Union[int, str] = INFO, formatter: Optional[Formatter] = None
+) -> Logger:
     logger: Logger = getLogger(name)
     level = environ.get("LOGLEVEL") or level
     logger.setLevel(level)
@@ -14,10 +16,13 @@ def get_logger(name: str, level: Union[int, str] = INFO) -> Logger:
     else:
         stream_handler: StreamHandler[TextIO] = StreamHandler(stderr)
         stream_handler.setLevel(level=level)
-        # noinspection SpellCheckingInspection
-        formatter: Formatter = Formatter(
-            "%(asctime)s.%(msecs)03d %(levelname)s %(module)s %(lineno)d - %(funcName)s: %(message)s"
-        )
+        if formatter is None:
+            # noinspection SpellCheckingInspection
+            # https://docs.python.org/3.1/library/logging.html#formatter-objects
+            formatter = Formatter(
+                "%(asctime)s %(levelname)s %(module)s.%(funcName)s[%(lineno)d]: %(message)s",
+                datefmt="%Y-%m-%d %H:%M:%S",
+            )
         stream_handler.setFormatter(formatter)
         logger.addHandler(stream_handler)
     return logger
