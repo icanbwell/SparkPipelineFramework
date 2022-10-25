9bf07ef47071d3a91f613ca8c0a7203af1871072
imranq2 <imranq2@hotmail.com>
2022-10-25 16:32:11 -0700

    move tests into folders

--- spark_pipeline_framework/utilities/file_downloader/test/test_file_downloader.py
+++ spark_pipeline_framework/utilities/file_downloader/test/test_file_downloader.py
@@ -1,5 +1,7 @@
 import os
+from os import path, makedirs
 from pathlib import Path
+from shutil import rmtree
 
 from spark_pipeline_framework.utilities.file_downloader.file_downloader import (
     FileDownloader,
@@ -8,8 +10,15 @@ from spark_pipeline_framework.utilities.file_downloader.file_downloader import (
 
 def test_file_downloader() -> None:
     # Arrange
+    data_dir: Path = Path(__file__).parent.joinpath("./")
+    temp_folder = data_dir.joinpath("./temp")
+    if path.isdir(temp_folder):
+        rmtree(temp_folder)
+    makedirs(temp_folder)
+    makedirs(temp_folder.joinpath("data"))
+
     file_url: str = "https://files.pythonhosted.org/packages/47/6a/62e288da7bcda82b935ff0c6cfe542970f04e29c756b0e147251b2fb251f/wget-3.2.zip"
-    download_path: str = f"file://{os.path.join(Path(__file__).parent, 'data')}"
+    download_path: str = f"file://{os.path.join(temp_folder, 'data')}"
 
     # Act
     downloader = FileDownloader(
