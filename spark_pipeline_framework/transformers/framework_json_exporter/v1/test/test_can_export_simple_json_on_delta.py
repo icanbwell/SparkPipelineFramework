7fb071a2550ec6dd6680dcc0a012b7788ee3d625
imranq2 <imranq2@hotmail.com>
2022-11-11 10:56:21 -0800

    add test for json exporter with delta

--- spark_pipeline_framework/transformers/framework_json_exporter/v1/test/test_can_export_simple_json_on_delta.py
+++ spark_pipeline_framework/transformers/framework_json_exporter/v1/test/test_can_export_simple_json_on_delta.py
@@ -14,37 +14,9 @@ from spark_pipeline_framework.transformers.framework_json_loader.v1.framework_js
 from tests.conftest import clean_spark_session
 
 
-def test_can_load_simple_json(spark_session: SparkSession) -> None:
-    # Arrange
-    clean_spark_session(spark_session)
-
-    data_dir: Path = Path(__file__).parent.joinpath("./")
-    test_file_path: str = f"{data_dir.joinpath('test.json')}"
-
-    schema = StructType([])
-
-    df: DataFrame = spark_session.createDataFrame(
-        spark_session.sparkContext.emptyRDD(), schema
-    )
-
-    # Act
-    FrameworkJsonLoader(view="books", file_path=test_file_path).transform(df)
-
-    # noinspection SqlDialectInspection
-    result: DataFrame = spark_session.sql("SELECT * FROM books")
-
-    result.show()
-
-    # Assert
-    assert result.count() == 2
-
-    assert result.collect()[1]["title"] == "Essentials of Programming Languages"
-    assert len(result.collect()[1]["authors"]) == 2
-    assert result.collect()[1]["authors"][0]["surname"] == "Friedman"
-    assert result.collect()[1]["edition"] == 3
-
-
-def test_can_export_simple_json_with_schema(spark_session: SparkSession) -> None:
+def test_can_export_simple_json_with_schema_on_delta(
+    spark_session: SparkSession,
+) -> None:
     # Arrange
     clean_spark_session(spark_session)
     data_dir: Path = Path(__file__).parent.joinpath("./")
@@ -76,11 +48,17 @@ def test_can_export_simple_json_with_schema(spark_session: SparkSession) -> None
     FrameworkJsonExporter(
         view="books_schema",
         file_path=temp_folder.joinpath("out.json"),
+        delta_lake_table="table1",
     ).transform(df)
+
+    df = spark_session.read.format("delta").load(str(temp_folder.joinpath("out.json")))
+    assert df.count() == 2
+    df.show(truncate=False)
 
     FrameworkJsonLoader(
         view="books_schema",
         file_path=temp_folder.joinpath("out.json"),
+        delta_lake_table="table1",
     ).transform(df)
 
     result_2 = spark_session.sql("SELECT * FROM books_schema")
