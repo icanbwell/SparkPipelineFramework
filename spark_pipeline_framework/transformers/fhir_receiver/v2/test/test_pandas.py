7f3b4b6161e9ccc78c3bfa8860653c68fbac9417
imranq2 <imranq2@hotmail.com>
2023-01-26 20:55:34 -0800

    add test for pandas

--- spark_pipeline_framework/transformers/fhir_receiver/v2/test/test_pandas.py
+++ spark_pipeline_framework/transformers/fhir_receiver/v2/test/test_pandas.py
@@ -1,5 +1,6 @@
 from typing import Iterable, Any
 
+from pyarrow import RecordBatch
 from pyspark.pandas import DataFrame
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, IntegerType
@@ -21,8 +22,11 @@ def test_pandas2(spark_session: SparkSession) -> None:
 
     def run_func(iterator: Iterable[DataFrame[Any]]) -> Iterable[DataFrame[Any]]:
         pdf: DataFrame[Any]
+        i: int = 0
         for pdf in iterator:
+            i = i + 1
             pdf["result"] = pdf.apply(lambda row: row[0] + row[1], axis=1)
+            print(f"loop: {i}")
             yield pdf
 
     response_schema = StructType(
@@ -33,4 +37,21 @@ def test_pandas2(spark_session: SparkSession) -> None:
         ]
     )
 
+    # Each pyarrow.RecordBatch size can be controlled by spark.sql.execution.arrow.maxRecordsPerBatch.
     df.mapInPandas(run_func, response_schema).show()
+
+
+def test_pandas3(spark_session: SparkSession) -> None:
+    df = spark_session.createDataFrame([(1, 21), (2, 30)], ("id", "age"))
+
+    def run_func(iterator: Iterable[RecordBatch]) -> Iterable[RecordBatch]:
+        pdf: RecordBatch
+        for pdf in iterator:
+            # pdf["result"] = pdf.apply(lambda row: row[0] + row[1], axis=1)
+            yield pdf
+
+    response_schema = StructType(
+        [StructField("id", IntegerType()), StructField("age", IntegerType())]
+    )
+
+    df.mapInArrow(run_func, response_schema).show()
