d1cec8fd72061e4cfdbd6687865fcdbf0f0a2dc6
imranq2 <imranq2@hotmail.com>
2023-01-26 21:10:58 -0800

    add test for pandas

--- spark_pipeline_framework/transformers/fhir_receiver/v2/test/test_pandas.py
+++ spark_pipeline_framework/transformers/fhir_receiver/v2/test/test_pandas.py
@@ -18,7 +18,8 @@ def test_pandas(spark_session: SparkSession) -> None:
 
 
 def test_pandas2(spark_session: SparkSession) -> None:
-    df = spark_session.createDataFrame([(1, 21), (2, 30)], ("id", "age"))
+    print("")
+    df = spark_session.createDataFrame([(1, 21), (2, 30), (3, 40)], ("id", "age"))
 
     def run_func(iterator: Iterable[DataFrame[Any]]) -> Iterable[DataFrame[Any]]:
         pdf: DataFrame[Any]
@@ -36,6 +37,11 @@ def test_pandas2(spark_session: SparkSession) -> None:
             StructField("result", IntegerType()),
         ]
     )
+
+    # https://www.databricks.com/blog/2022/02/02/scaling-shap-calculations-with-pyspark-and-pandas-udf.html
+    # df = df.repartition(sc.defaultParallelism)
+    df = df.repartition(3)
+    print(f"partitions={df.rdd.getNumPartitions()}")
 
     # Each pyarrow.RecordBatch size can be controlled by spark.sql.execution.arrow.maxRecordsPerBatch.
     df.mapInPandas(run_func, response_schema).show()
