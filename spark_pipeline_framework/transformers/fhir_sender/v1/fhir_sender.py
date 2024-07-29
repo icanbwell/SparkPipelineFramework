9ae8655387d3e751ff200c140044d5ffc3b0a3cd
Imran Qureshi <imranq2@hotmail.com>
2024-07-29 14:10:24 -0700

    fix tests

--- spark_pipeline_framework/transformers/fhir_sender/v1/fhir_sender.py
+++ spark_pipeline_framework/transformers/fhir_sender/v1/fhir_sender.py
@@ -67,6 +67,7 @@ class FhirSender(FrameworkTransformer):
         auth_client_secret: Optional[str] = None,
         auth_login_token: Optional[str] = None,
         auth_scopes: Optional[List[str]] = None,
+        auth_well_known_url: Optional[str] = None,
         operation: Union[
             FhirSenderOperation, str
         ] = FhirSenderOperation.FHIR_OPERATION_MERGE.value,
@@ -195,6 +196,11 @@ class FhirSender(FrameworkTransformer):
         self.auth_scopes: Param[List[str]] = Param(self, "auth_scopes", "")
         self._setDefault(auth_scopes=None)
 
+        self.auth_well_known_url: Param[Optional[str]] = Param(
+            self, "auth_well_known_url", ""
+        )
+        self._setDefault(auth_well_known_url=None)
+
         self.operation: Param[Union[FhirSenderOperation, str]] = Param(
             self, "operation", ""
         )
@@ -317,6 +323,7 @@ class FhirSender(FrameworkTransformer):
         file_format: str = self.getFileFormat()
         validation_server_url: Optional[str] = self.getValidationServerUrl()
         auth_server_url: Optional[str] = self.getAuthServerUrl()
+        auth_well_known_url: Optional[str] = self.getOrDefault(self.auth_well_known_url)
         auth_client_id: Optional[str] = self.getAuthClientId()
         auth_client_secret: Optional[str] = self.getAuthClientSecret()
         auth_login_token: Optional[str] = self.getAuthLoginToken()
@@ -376,6 +383,7 @@ class FhirSender(FrameworkTransformer):
                 auth_login_token=auth_login_token,
                 auth_scopes=auth_scopes,
                 log_level=log_level,
+                auth_well_known_url=auth_well_known_url,
             )
 
         self.logger.info(
--- spark_pipeline_framework/transformers/fhir_sender/v1/test/test_fhir_sender_merge.py
+++ spark_pipeline_framework/transformers/fhir_sender/v1/test/test_fhir_sender_merge.py
@@ -2,12 +2,14 @@ import json
 from os import path, makedirs, environ
 from pathlib import Path
 from shutil import rmtree
+from urllib.parse import urljoin
 
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.types import IntegerType
 
 from spark_pipeline_framework.progress_logger.progress_logger import ProgressLogger
 from spark_pipeline_framework.transformers.fhir_sender.v1.fhir_sender import FhirSender
+from spark_pipeline_framework.utilities.fhir_helpers.token_helper import TokenHelper
 from spark_pipeline_framework.utilities.spark_data_frame_helpers import (
     create_empty_dataframe,
 )
@@ -29,7 +31,15 @@ def test_fhir_sender_merge(spark_session: SparkSession) -> None:
 
     df: DataFrame = create_empty_dataframe(spark_session=spark_session)
 
-    fhir_server_url: str = "http://fhir:3000/4_0_0"
+    fhir_server_url: str = environ["FHIR_SERVER_URL"]
+    auth_client_id = environ["FHIR_CLIENT_ID"]
+    auth_client_secret = environ["FHIR_CLIENT_SECRET"]
+    auth_well_known_url = environ["AUTH_CONFIGURATION_URI"]
+    token_url = TokenHelper.get_auth_server_url_from_well_known_url(
+        well_known_url=auth_well_known_url
+    )
+    assert token_url
+    authorization_header = TokenHelper.get_authorization_header_from_environment()
 
     environ["LOGLEVEL"] = "DEBUG"
     # Act
@@ -47,13 +57,13 @@ def test_fhir_sender_merge(spark_session: SparkSession) -> None:
         ).transform(df)
 
     # Assert
-    response = requests.get(f"{fhir_server_url}/Patient/00100000000")
+    response = requests.get(urljoin(fhir_server_url, "Patient/00100000000"))
     assert response.ok, response.text
     json_text: str = response.text
     obj = json.loads(json_text)
     assert obj["birthDate"] == "2017-01-01"
 
-    response = requests.get(f"{fhir_server_url}/Patient/00200000000")
+    response = requests.get(urljoin(fhir_server_url, "Patient/00200000000"))
     assert response.ok, response.text
     json_text = response.text
     obj = json.loads(json_text)
@@ -73,7 +83,15 @@ def test_fhir_sender_merge_for_custom_parameters(spark_session: SparkSession) ->
 
     df: DataFrame = create_empty_dataframe(spark_session=spark_session)
 
-    fhir_server_url: str = "http://fhir:3000/4_0_0"
+    fhir_server_url: str = environ["FHIR_SERVER_URL"]
+    auth_client_id = environ["FHIR_CLIENT_ID"]
+    auth_client_secret = environ["FHIR_CLIENT_SECRET"]
+    auth_well_known_url = environ["AUTH_CONFIGURATION_URI"]
+    token_url = TokenHelper.get_auth_server_url_from_well_known_url(
+        well_known_url=auth_well_known_url
+    )
+    assert token_url
+    authorization_header = TokenHelper.get_authorization_header_from_environment()
 
     environ["LOGLEVEL"] = "DEBUG"
     # Act
@@ -92,7 +110,9 @@ def test_fhir_sender_merge_for_custom_parameters(spark_session: SparkSession) ->
         ).transform(df)
 
         # for first EOB
-        response = requests.get(f"{fhir_server_url}/ExplanationOfBenefit/H111-12345")
+        response = requests.get(
+            urljoin(fhir_server_url, "ExplanationOfBenefit/H111-12345")
+        )
         assert response.ok, response.text
         json_text = response.text
         obj = json.loads(json_text)
@@ -101,7 +121,9 @@ def test_fhir_sender_merge_for_custom_parameters(spark_session: SparkSession) ->
         assert obj.get("source_file_line_num") is None
 
         # for second EOB
-        response = requests.get(f"{fhir_server_url}/ExplanationOfBenefit/H222-12345")
+        response = requests.get(
+            urljoin(fhir_server_url, "ExplanationOfBenefit/H222-12345")
+        )
         assert response.ok, response.text
         json_text = response.text
         obj = json.loads(json_text)
@@ -110,7 +132,9 @@ def test_fhir_sender_merge_for_custom_parameters(spark_session: SparkSession) ->
         assert obj.get("source_file_line_num") is None
 
         # for third EOB
-        response = requests.get(f"{fhir_server_url}/ExplanationOfBenefit/H333-12345")
+        response = requests.get(
+            urljoin(fhir_server_url, "ExplanationOfBenefit/H333-12345")
+        )
         assert response.ok, response.text
         json_text = response.text
         obj = json.loads(json_text)
