445ed7ba53807cd31807f61ec4394154d67bf41f
Imran Qureshi <imranq2@hotmail.com>
2024-07-28 17:50:59 -0700

    add test for elasticsearch_sender.py

---
+++ spark_pipeline_framework/transformers/elasticsearch_sender/v2/test/test_elasticsearch_sender.py
@@ -0,0 +1,50 @@
+from typing import List, Tuple
+
+from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql.types import StructType, StructField, StringType
+
+from spark_pipeline_framework.progress_logger.progress_logger import ProgressLogger
+from spark_pipeline_framework.transformers.elasticsearch_sender.v2.elasticsearch_sender import (
+    ElasticSearchSender,
+)
+
+
+def test_elasticsearch_sender(spark_session: SparkSession) -> None:
+    # Given
+    data: List[Tuple[str, str]] = [
+        ("1", "{'id': '1', 'name': 'name1'}"),
+        ("2", "{'id': '2', 'name': 'name2'}"),
+    ]
+    schema: StructType = StructType(
+        [StructField("id", StringType()), StructField("json", StringType())]
+    )
+    df: DataFrame = spark_session.createDataFrame(data, schema)
+    view: str = "test_view"
+    df.createOrReplaceTempView(view)
+
+    elasticsearch_sender: ElasticSearchSender = ElasticSearchSender(
+        name="elasticsearch_sender",
+        view=view,
+        index="test_index",
+        operation="index",
+        parameters={"doc_id_prefix": "test"},
+        progress_logger=ProgressLogger(),
+    )
+
+    # When
+    result: DataFrame = elasticsearch_sender.transform(df)
+
+    # Then
+    assert result.count() == 2
+    assert result.columns == [
+        "_index",
+        "_id",
+        "_source",
+        "_op_type",
+        "_version",
+        "_seq_no",
+        "_primary_term",
+        "result",
+    ]
+    assert result.collect()[0]["result"] == "created"
+    assert result.collect()[1]["result"] == "created"
