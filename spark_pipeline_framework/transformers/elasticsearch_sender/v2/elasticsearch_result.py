8d0a86b43b3dc4bc162d36726d88441a3ddebacb
Imran Qureshi <imranq2@hotmail.com>
2024-07-30 20:19:11 -0700

    flatten payload so it matches spark schema

--- spark_pipeline_framework/transformers/elasticsearch_sender/v2/elasticsearch_result.py
+++ spark_pipeline_framework/transformers/elasticsearch_sender/v2/elasticsearch_result.py
@@ -1,3 +1,4 @@
+import json
 from dataclasses import dataclass
 from typing import Any, Dict, List
 
@@ -6,18 +7,29 @@ from pyspark.sql.types import (
     StructField,
     StringType,
     IntegerType,
-    ArrayType,
-    MapType,
 )
 
 
 @dataclass
 class ElasticSearchResult:
+    """
+    Represents the result of an ElasticSearch operation
+    """
+
     url: str
+    """  The url of the ElasticSearch server """
+
     success: int
+    """ The number of successful operations """
+
     failed: int
+    """ The number of failed operations """
+
     payload: List[Dict[str, Any]]
+    """ The payload that was sent to the server """
+
     partition_index: int
+    """ The index of the partition """
 
     def to_dict(self) -> Dict[str, Any]:
         return {
@@ -28,16 +40,30 @@ class ElasticSearchResult:
             "partition_index": self.partition_index,
         }
 
+    def to_dict_flatten_payload(self) -> Dict[str, Any]:
+        """
+        Returns a dictionary with the payload as a json string
+
+        """
+        return {
+            "url": self.url,
+            "success": self.success,
+            "failed": self.failed,
+            "partition_index": self.partition_index,
+            "payload": json.dumps(self.payload),
+        }
+
     @staticmethod
     def get_schema() -> StructType:
+        """
+        Returns the schema for the ElasticSearchResult
+        """
         return StructType(
             [
                 StructField("url", StringType(), True),
                 StructField("success", IntegerType(), True),
                 StructField("failed", IntegerType(), True),
-                StructField(
-                    "payload", ArrayType(MapType(StringType(), StringType())), True
-                ),
+                StructField("payload", StringType(), True),
                 StructField("partition_index", IntegerType(), True),
             ]
         )
