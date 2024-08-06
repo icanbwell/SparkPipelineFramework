f4a757eac80110cca9422e5064d4b6e1e0292942
Imran Qureshi <imranq2@hotmail.com>
2024-08-06 10:22:48 -0700

    add dynamic_class_loader.py

--- spark_pipeline_framework/utilities/dynamic_class_loader/v1/test/test_dynamic_class_loader.py
+++ spark_pipeline_framework/utilities/dynamic_class_loader/v1/test/test_dynamic_class_loader.py
@@ -11,10 +11,13 @@ from spark_pipeline_framework.utilities.helix_geolocation.v1.standardizing_vendo
 
 def test_dynamic_class_loader() -> None:
     data_dir: Path = Path(__file__).parent.joinpath("./")
-    standardizing_vendor_path = data_dir.joinpath("./././helix_geolocation/v1/vendors")
+    standardizing_vendor_path = data_dir.joinpath(
+        "../../../helix_geolocation/v1/vendors"
+    )
     sub_classes: List[Type[StandardizingVendor]] = DynamicClassLoader[
         StandardizingVendor
     ](StandardizingVendor, standardizing_vendor_path).find_subclasses()
-    assert len(sub_classes) == 2
-    assert sub_classes[0].__name__ == "Vendor1"
-    assert sub_classes[1].__name__ == "Vendor2"
+    assert len(sub_classes) == 3
+    assert sub_classes[0].__name__ == "GeocodioStandardizingVendor"
+    assert sub_classes[1].__name__ == "MelissaStandardizingVendor"
+    assert sub_classes[2].__name__ == "MockStandardizingVendor"
