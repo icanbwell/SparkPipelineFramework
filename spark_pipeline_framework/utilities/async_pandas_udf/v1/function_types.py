9eaef0fd1507b980f865390ce4b71d6973bd1e39
Imran Qureshi <imranq2@hotmail.com>
2024-08-06 22:03:47 -0700

    add async_pandas_dataframe_udf.py

--- spark_pipeline_framework/utilities/async_pandas_udf/v1/function_types.py
+++ spark_pipeline_framework/utilities/async_pandas_udf/v1/function_types.py
@@ -1,10 +1,28 @@
-from typing import Protocol, List, Dict, Any
+from typing import Protocol, List, Dict, Any, Optional, TypeVar
 
 
 class HandlePandasBatchFunction(Protocol):
     async def __call__(
         self,
         input_values: List[Dict[str, Any]],
+    ) -> List[Dict[str, Any]]:
+        """
+        This function is called with a batch of input values and should return a batch of output values.
+
+        :param input_values: input values as a list of dictionaries
+        :return: output values as a list of dictionaries
+        """
+        ...
+
+
+# By declaring T as contravariant=True, you indicate that T can be a more general type than the actual type used,
+# which aligns with how Protocol expects type variables to be used in this context.
+T = TypeVar("T", contravariant=True)
+
+
+class HandlePandasBatchWithParametersFunction(Protocol[T]):
+    async def __call__(
+        self, input_values: List[Dict[str, Any]], parameters: Optional[T]
     ) -> List[Dict[str, Any]]:
         """
         This function is called with a batch of input values and should return a batch of output values.
