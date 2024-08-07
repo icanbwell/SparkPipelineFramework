590192549baaeb467e7f8aa456e9f5f1ac956161
Imran Qureshi <imranq2@hotmail.com>
2024-08-07 12:50:51 -0700

    fix test

--- spark_pipeline_framework/utilities/dynamic_class_loader/v1/test/test_dynamic_class_loader.py
+++ spark_pipeline_framework/utilities/dynamic_class_loader/v1/test/test_dynamic_class_loader.py
@@ -4,19 +4,23 @@ from typing import Type, List
 from spark_pipeline_framework.utilities.dynamic_class_loader.v1.dynamic_class_loader import (
     DynamicClassLoader,
 )
-from spark_pipeline_framework.utilities.helix_geolocation.v1.standardizing_vendor import (
+from spark_pipeline_framework.utilities.helix_geolocation.v2.standardizing_vendor import (
     StandardizingVendor,
 )
 
 
 def test_dynamic_class_loader() -> None:
+    print("")
     data_dir: Path = Path(__file__).parent.joinpath("./")
     standardizing_vendor_path = data_dir.joinpath(
         "../../../helix_geolocation/v2/vendors"
     )
+    print(f"standardizing_vendor_path: {standardizing_vendor_path}")
     sub_classes: List[Type[StandardizingVendor]] = DynamicClassLoader[
         StandardizingVendor
-    ](StandardizingVendor, standardizing_vendor_path).find_subclasses()
+    ](
+        StandardizingVendor, standardizing_vendor_path  # type: ignore[type-abstract]
+    ).find_subclasses()
     assert len(sub_classes) == 4
 
     assert sorted([sub_class.__name__ for sub_class in sub_classes]) == sorted(
