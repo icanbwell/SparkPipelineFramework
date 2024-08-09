cb0d8920674805a8b508e6d35233e83b53427657
Imran Qureshi <imranq2@hotmail.com>
2024-08-08 22:34:20 -0700

    fix data_class_loader.py

--- spark_pipeline_framework/utilities/data_class_loader/v1/data_class_loader.py
+++ spark_pipeline_framework/utilities/data_class_loader/v1/data_class_loader.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from dataclasses import fields, MISSING
-from typing import Any, Dict, Type, TypeVar, TYPE_CHECKING, get_origin, get_args
+from typing import Any, Dict, Type, TypeVar, TYPE_CHECKING, get_origin, get_args, Union
 
 if TYPE_CHECKING:
     # noinspection PyUnresolvedReferences
@@ -21,15 +21,33 @@ class DataClassLoader:
             field_value = data.get(field_name, MISSING)
 
             if field_value is not MISSING:
+                # Handle Optional by unwrapping it
+                origin_type = get_origin(field_type)
+                if origin_type is Union and type(None) in get_args(field_type):
+                    field_type = next(
+                        t for t in get_args(field_type) if t is not type(None)
+                    )
+                    origin_type = get_origin(field_type)
+
                 # Check if the field is a list
-                if get_origin(field_type) is list:
+                if origin_type is list:
                     item_type = get_args(field_type)[0]
                     if hasattr(item_type, "__dataclass_fields__"):
-                        # noinspection PyTypeChecker
                         field_values[field_name] = [
                             DataClassLoader.from_dict(item_type, item)
                             for item in field_value
                         ]
+                    else:
+                        field_values[field_name] = field_value
+
+                # Check if the field is a dict
+                elif origin_type is dict:
+                    key_type, value_type = get_args(field_type)
+                    if hasattr(value_type, "__dataclass_fields__"):
+                        field_values[field_name] = {  # type: ignore[assignment]
+                            key: DataClassLoader.from_dict(value_type, val)
+                            for key, val in field_value
+                        }
                     else:
                         field_values[field_name] = field_value
 
@@ -43,5 +61,8 @@ class DataClassLoader:
 
                 else:
                     field_values[field_name] = field_value
+
+            elif get_origin(field_type) is Union and type(None) in get_args(field_type):
+                field_values[field_name] = []
 
         return data_class(**field_values)
