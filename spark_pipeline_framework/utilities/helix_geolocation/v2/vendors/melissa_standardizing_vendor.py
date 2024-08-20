5a6a891641ab97ae91bdadc44cca2671ccd0580f
Shubham <shubham.goel@icanbwell.com>
2024-08-20 19:16:30 +0530

    Removing assert statement

--- spark_pipeline_framework/utilities/helix_geolocation/v2/vendors/melissa_standardizing_vendor.py
+++ spark_pipeline_framework/utilities/helix_geolocation/v2/vendors/melissa_standardizing_vendor.py
@@ -72,9 +72,11 @@ class MelissaStandardizingVendor(
                                             until we cancel rest of requests
         """
         super().__init__(version)
-        assert (
-            license_key or custom_api_call
-        ), "Either license_key or custom_api_call must be provided"
+        # commenting out as this is creating an issue in pipeline_loader.py when the environment variables
+        # are getting initialized
+        # assert (
+        #     license_key or custom_api_call
+        # ), "Either license_key or custom_api_call must be provided"
         self._license_key: str = license_key
         self._custom_api_call_async: Optional[CustomApiCallFunction] = custom_api_call
         self._error_counter: int = 0
