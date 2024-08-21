b2029b2ed191718efeb8c38d8daee15a7b56d5d7
Imran Qureshi <imranq2@hotmail.com>
2024-08-21 09:07:14 -0700

    remove library folder from package

--- setup.py
+++ setup.py
@@ -28,7 +28,22 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/icanbwell/SparkPipelineFramework",
-    packages=find_packages(exclude=["**/test", "**/test/**", "library/**", "library"]),
+    packages=find_packages(
+        exclude=[
+            "**/test",
+            "**/test/**",
+            "library/**",
+            "library",
+            "docs",
+            "docs/**",
+            "docsrc",
+            "docsrc/**",
+            "keycloak-config",
+            "keycloak-config/**",
+            ".gihub",
+            ".github/**",
+        ]
+    ),
     install_requires=[
         "protobuf>=3",
         "pyspark==3.5.1",
