7a0a3a473ce71094704070a97346df145c2242f2
Imran Qureshi <imranq2@hotmail.com>
2024-09-30 15:06:45 -0700

    use env var

--- tests/iterable/iterable_helper.py
+++ tests/iterable/iterable_helper.py
@@ -1,4 +1,5 @@
 import json
+import os
 from typing import Iterable, Any, Dict, List
 
 import pandas as pd
@@ -18,7 +19,7 @@ class IterableHelper:
         ) -> Iterable[pd.DataFrame]:
             # https://support.iterable.com/hc/en-us/articles/204780579-API-Overview-and-Sample-Payloads#post-api-users-update
             api_url = "https://api.iterable.com/api/users/update"  # Replace with your actual API URL
-            iterable_api_key = "79b444d11d8e4e97bc04c7d0c60ab27d"
+            iterable_api_key = os.getenv("ITERABLE_API_KEY")
 
             batch: pd.DataFrame
             for batch in batch_iter:
@@ -88,6 +89,104 @@ class IterableHelper:
 
         # Use mapInPandas to apply the API call to each partition of the DataFrame
         result_df = user_profile_df.mapInPandas(call_api_for_each_row, schema)
+
+        # Show the resulting DataFrame
+        result_df.show()
+
+    @staticmethod
+    def send_task_to_iterable(task_df: DataFrame) -> None:
+        print(f"Task count:{task_df.count()}")
+
+        # Define function to call REST API
+        def call_api_for_each_row(
+            batch_iter: Iterable[pd.DataFrame],
+        ) -> Iterable[pd.DataFrame]:
+            # https://support.iterable.com/hc/en-us/articles/204780579-API-Overview-and-Sample-Payloads#post-api-users-update
+            api_url = "https://api.iterable.com/api/events/track"  # Replace with your actual API URL
+            iterable_api_key = os.getenv("ITERABLE_API_KEY")
+
+            batch: pd.DataFrame
+            for batch in batch_iter:
+                responses = []
+
+                pdf_json: str = batch.to_json(orient="records")
+                input_rows: List[Dict[str, Any]] = json.loads(pdf_json)
+
+                result: List[Dict[str, Any]] = []
+
+                for row in input_rows:
+                    master_person_id = row["master_person_id"]
+                    client_person_id = row["client_person_id"]
+                    organization_id = row["organization_id"]
+                    client_slug = row["client_slug"]
+                    created_date = row["created_date"]
+                    last_updated_date = row["last_updated_date"]
+                    activity_definition_id = row["activity_definition_id"]
+                    task_name = row["task_name"]
+                    task_id = row["task_id"]
+                    completed_date = row["completed_date"]
+                    all_fields: List[Dict[str, Any]] = row["all_fields"]
+
+                    try:
+                        headers = {
+                            "Api-key": iterable_api_key,
+                            "Content-Type": "application/json",
+                        }
+
+                        data_fields: Dict[str, Any] = {
+                            field["field_name"]: field["field_value"]
+                            for field in all_fields
+                        }
+                        data_fields["task_id"] = task_id
+                        data_fields["task_name"] = task_name
+                        data_fields["activity_definition_id"] = activity_definition_id
+                        data_fields["completed_date"] = completed_date
+
+                        data = {
+                            "userId": f"{master_person_id}",
+                            "eventName": (
+                                task_name
+                                if not completed_date
+                                else f"{task_name}_Completed"
+                            ),
+                            "id": task_id,
+                            "createdAt": created_date,
+                            "dataFields": data_fields,
+                            "campaignId": 0,
+                            "templateId": 0,
+                            "createNewFields": True,
+                        }
+                        print(f"data: {data}")
+                        # Make the POST request
+                        response = requests.post(api_url, headers=headers, json=data)
+                        # Get the response text (or extract relevant info as needed)
+                        responses.append(response.text)
+                    except requests.exceptions.RequestException as e:
+                        # Handle errors, you could log or return an error message
+                        responses.append(f"Error: {str(e)}")
+
+                # Add the responses to the DataFrame
+                batch["api_response"] = responses
+
+            return batch_iter
+
+        # Define schema
+        schema = StructType(
+            [
+                StructField("master_person_id", StringType(), False),
+                StructField("client_person_id", StringType(), False),
+                StructField("organization_id", StringType(), False),
+                StructField("client_slug", StringType(), False),
+                StructField("created_date", TimestampType(), False),
+                StructField("last_updated_date", TimestampType(), False),
+                StructField(
+                    "api_response", StringType(), True
+                ),  # Field to store API response
+            ]
+        )
+
+        # Use mapInPandas to apply the API call to each partition of the DataFrame
+        result_df = task_df.mapInPandas(call_api_for_each_row, schema)
 
         # Show the resulting DataFrame
         result_df.show()
--- tests/iterable/test_iterable.py
+++ tests/iterable/test_iterable.py
@@ -11,44 +11,94 @@ from tests.iterable.iterable_helper import IterableHelper
 from tests.iterable.tables_lister import get_list_of_tables
 
 
-def test_iterable(spark_session: SparkSession) -> None:
-    clean_spark_session(spark_session)
-    # Arrange
+def test_loading_user_profile_into_iterable(spark_session: SparkSession) -> None:
     data_dir: Path = Path(__file__).parent.joinpath("./")
 
-    df: DataFrame = create_empty_dataframe(spark_session=spark_session)
+    setup_schema(spark_session)
 
-    tables: List[Dict[str, Any]] = get_list_of_tables(spark_session)
+    load_test_files(data_dir, spark_session)
 
-    assert len(tables) == 0
+    show_tables(spark_session)
 
-    sql_path = data_dir.joinpath("sql")
+    # Use SQL to join the tables while avoiding duplicate columns
+    query = """
+        SELECT 
+            up.master_person_id,
+            up.client_person_id,
+            up.organization_id,
+            up.client_slug,
+            up.created_date,
+            up.last_updated_date,
+            collect_list(struct(upf.field_name, upf.field_value)) AS all_fields
+        FROM business_events.UserProfile up
+        LEFT JOIN business_events.UserProfileFields upf
+        ON up.master_person_id = upf.master_person_id
+        AND up.client_person_id = upf.client_person_id
+        AND up.organization_id = upf.organization_id
+        AND up.client_slug = upf.client_slug
+        GROUP BY
+            up.master_person_id,
+            up.client_person_id,
+            up.organization_id,
+            up.client_slug,
+            up.created_date,
+            up.last_updated_date
+    """
 
-    sql_files: List[str] = [
-        "user_profile_schema.sql",
-        "user_profile.sql",
-        "user_profile_fields.sql",
-        "activity_definition.sql",
-        "tasks.sql",
-        "task_fields.sql",
-        "business_events.sql",
-        "business_event_fields.sql",
-    ]
+    # Execute the query and create a DataFrame
+    user_profile_with_fields_df = spark_session.sql(query)
 
-    for sql_file in sql_files:
-        with open(sql_path.joinpath(sql_file), "r") as file:
-            sql_text = file.read()
-            df.sparkSession.sql(sql_text)
+    IterableHelper.send_user_profile_to_iterable(
+        user_profile_df=user_profile_with_fields_df
+    )
 
+    # Now process Tasks
+    query = """
+        SELECT 
+            t.master_person_id,
+            t.client_person_id,
+            t.organization_id,
+            t.client_slug,
+            t.created_date,
+            t.last_updated_date,
+            t.activity_definition_id,
+            t.task_name,
+            t.task_id,
+            t.completed_date,
+            collect_list(struct(tf.field_name, tf.field_value)) AS all_fields
+        FROM business_events.Tasks t
+        LEFT JOIN business_events.TaskFields tf
+        ON t.master_person_id = tf.master_person_id
+        AND t.client_person_id = tf.client_person_id
+        AND t.organization_id = tf.organization_id
+        AND t.client_slug = tf.client_slug
+        AND t.task_id = tf.task_id
+        GROUP BY
+            t.master_person_id,
+            t.client_person_id,
+            t.organization_id,
+            t.client_slug,
+            t.created_date,
+            t.last_updated_date,
+            t.task_id
+    """
+
+    tasks_with_fields_df = spark_session.sql(query)
+
+    IterableHelper.send_task_to_iterable(task_df=tasks_with_fields_df)
+
+
+def show_tables(spark_session: SparkSession) -> None:
     tables = get_list_of_tables(spark_session)
+    for table in tables:
+        print(table)
+        table_name = table["table_name"]
+        spark_session.read.format("delta").table(f"business_events.{table_name}").show()
 
-    print(tables)
 
-    assert len(tables) == 7
-
+def load_test_files(data_dir: Path, spark_session: SparkSession) -> None:
     # Add sample data
     test_files_path: Path = data_dir.joinpath("test_files")
-
     test_files: List[str] = [
         "user_profile.csv",
         "user_profile_fields.csv",
@@ -77,38 +127,29 @@ def test_iterable(spark_session: SparkSession) -> None:
             f"business_events.{table_name}"
         )
 
+
+def setup_schema(spark_session: SparkSession) -> None:
+    clean_spark_session(spark_session)
+    # Arrange
+    data_dir: Path = Path(__file__).parent.joinpath("./")
+    df: DataFrame = create_empty_dataframe(spark_session=spark_session)
+    tables: List[Dict[str, Any]] = get_list_of_tables(spark_session)
+    assert len(tables) == 0
+    sql_path = data_dir.joinpath("sql")
+    sql_files: List[str] = [
+        "user_profile_schema.sql",
+        "user_profile.sql",
+        "user_profile_fields.sql",
+        "activity_definition.sql",
+        "tasks.sql",
+        "task_fields.sql",
+        "business_events.sql",
+        "business_event_fields.sql",
+    ]
+    for sql_file in sql_files:
+        with open(sql_path.joinpath(sql_file), "r") as file:
+            sql_text = file.read()
+            df.sparkSession.sql(sql_text)
     tables = get_list_of_tables(spark_session)
-    for table in tables:
-        print(table)
-        table_name = table["table_name"]
-        spark_session.read.format("delta").table(f"business_events.{table_name}").show()
-
-    # Use SQL to join the tables while avoiding duplicate columns
-    query = """
-        SELECT 
-            up.master_person_id,
-            up.client_person_id,
-            up.organization_id,
-            up.client_slug,
-            up.created_date,
-            up.last_updated_date,
-            collect_list(struct(upf.field_name, upf.field_value)) AS all_fields
-        FROM business_events.UserProfile up
-        LEFT JOIN business_events.UserProfileFields upf
-        ON up.master_person_id = upf.master_person_id
-        AND up.client_person_id = upf.client_person_id
-        AND up.organization_id = upf.organization_id
-        AND up.client_slug = upf.client_slug
-        GROUP BY
-            up.master_person_id,
-            up.client_person_id,
-            up.organization_id,
-            up.client_slug,
-            up.created_date,
-            up.last_updated_date
-    """
-
-    # Execute the query and create a DataFrame
-    result_df = spark_session.sql(query)
-
-    IterableHelper.send_user_profile_to_iterable(user_profile_df=result_df)
+    print(tables)
+    assert len(tables) == 7
