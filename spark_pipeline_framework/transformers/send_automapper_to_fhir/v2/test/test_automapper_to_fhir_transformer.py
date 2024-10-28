213821ae6546c171d187892b56bf677f123df7db
Imran Qureshi <imranq2@hotmail.com>
2024-10-28 15:49:47 -0700

    add test for automapper_to_fhir

---
+++ spark_pipeline_framework/transformers/send_automapper_to_fhir/v2/test/test_automapper_to_fhir_transformer.py
@@ -0,0 +1,78 @@
+from pathlib import Path
+from typing import Any, Dict
+
+from pyspark.sql import SparkSession, DataFrame
+
+from library.features.patients_fhir.v1.features_patients_fhir_v1 import (
+    FeaturesPatientsFhirV1,
+)
+from spark_pipeline_framework.progress_logger.progress_logger import ProgressLogger
+from spark_pipeline_framework.transformers.send_automapper_to_fhir.v2.automapper_to_fhir_transformer import (
+    AutoMapperToFhirTransformer,
+)
+from spark_pipeline_framework.utilities.spark_data_frame_helpers import (
+    create_empty_dataframe,
+)
+
+
+async def test_automapper_to_fhir_transformer_async(
+    spark_session: SparkSession,
+) -> None:
+    df: DataFrame = create_empty_dataframe(spark_session=spark_session)
+
+    data_dir: Path = Path(__file__).parent.joinpath("./")
+
+    def get_fhir_path(view: str, resource_name: str) -> str:
+        return str(
+            data_dir.joinpath("temp")
+            .joinpath("output")
+            .joinpath(f"{resource_name}-{view}")
+        )
+
+    def get_fhir_response_path(view: str, resource_name: str) -> str:
+        return str(
+            data_dir.joinpath("temp")
+            .joinpath("output")
+            .joinpath(f"{resource_name}-{view}-response")
+        )
+
+    # create the input dataframe
+    patients_df: DataFrame = spark_session.createDataFrame(
+        [  # type: ignore
+            {
+                "member_id": "1",
+                "last_name": "Doe",
+                "first_name": "John",
+            },
+            {
+                "member_id": "2",
+                "last_name": "Smith",
+                "first_name": "Jane",
+            },
+        ]
+    )
+    patients_df.createOrReplaceTempView("patients")
+
+    parameters: Dict[str, Any] = {}
+    with ProgressLogger() as progress_logger:
+        transformer = AutoMapperToFhirTransformer(
+            name="AutoMapperToFhirTransformer",
+            parameters=parameters,
+            progress_logger=progress_logger,
+            transformer=FeaturesPatientsFhirV1(
+                parameters={
+                    "source_view": "patients",
+                    "view": "fhir_patients",
+                },
+                progress_logger=progress_logger,
+            ),
+            func_get_path=get_fhir_path,
+            func_get_response_path=get_fhir_response_path,
+            fhir_server_url="http://mock-server:1080",
+            source_entity_name="Patient",
+        )
+        result_df = await transformer.transform_async(df)
+
+        result_df.show()
+
+        assert result_df.count() == 0
