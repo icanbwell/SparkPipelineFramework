5f599833a5fa145bec6aedeac9cbbc532c79be6e
Imran Qureshi <imranq2@hotmail.com>
2024-10-28 15:08:57 -0700

    fix async test for progress logger

--- spark_pipeline_framework/progress_logger/test/test_progress_logger_async.py
+++ spark_pipeline_framework/progress_logger/test/test_progress_logger_async.py
@@ -349,7 +349,7 @@ def test_progress_logger_mlflow_error_handling(test_setup: Any) -> None:
     runs: Union[List[Run], "pandas.DataFrame"] = mlflow.search_runs(
         experiment_ids=[experiment.experiment_id], output_format="list"
     )
-    if isinstance(runs, list):
+    if isinstance(runs, list) and len(runs) > 0:
         run: mlflow.entities.Run = runs[0]  # Run object
         assert run.info.status == RunStatus.to_string(RunStatus.FINISHED)  # type: ignore
 
