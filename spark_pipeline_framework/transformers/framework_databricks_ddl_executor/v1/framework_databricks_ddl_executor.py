64ba463bb4ac5515eb86877860c361afd4f612fc
Sumit Lohan <sumit.lohan@icanbwell.com>
2025-07-22 21:27:09 +0530

    RNGR-624 replaced print with logger.error

--- spark_pipeline_framework/transformers/framework_databricks_ddl_executor/v1/framework_databricks_ddl_executor.py
+++ spark_pipeline_framework/transformers/framework_databricks_ddl_executor/v1/framework_databricks_ddl_executor.py
@@ -7,6 +7,20 @@ from spark_pipeline_framework.transformers.framework_transformer.v1.framework_tr
 
 
 class FrameworkDatabricksDDLExecutor(FrameworkTransformer):
+    """
+    Executes a Data Definition Language (DDL) statement on a Databricks database using a JDBC connection.
+    This transformer is designed to execute DDL statements (e.g., CREATE, DROP, ALTER) on a database
+    connected via JDBC. It is intended to be used within a Spark pipeline.
+    Parameters:
+        jdbc_url (str): The JDBC URL for connecting to the database.
+        ddl (str): The DDL statement to execute. Ensure that this statement is validated to prevent SQL injection.
+        name (Optional[str]): An optional name for the transformer.
+
+    Important considerations:
+    - Be cautious when executing DDL statements, as they can modify the database schema and potentially
+      lead to data loss or corruption.
+    """
+
     def __init__(
         self,
         jdbc_url: str,
@@ -27,17 +41,17 @@ class FrameworkDatabricksDDLExecutor(FrameworkTransformer):
             stmt = conn.createStatement()
             stmt.execute(self.ddl)
         except Exception as e:
-            print(f"Error executing DDL: {e}")
+            self.logger.error(f"Error executing DDL: {e}")
             raise RuntimeError(f"Failed to execute DDL: {e}")
         finally:
             if stmt is not None:
                 try:
                     stmt.close()
                 except Exception as close_stmt_exc:
-                    print(f"Error closing statement: {close_stmt_exc}")
+                    self.logger.error(f"Error closing statement: {close_stmt_exc}")
             if conn is not None:
                 try:
                     conn.close()
                 except Exception as close_conn_exc:
-                    print(f"Error closing connection: {close_conn_exc}")
+                    self.logger.error(f"Error closing connection: {close_conn_exc}")
         return df
